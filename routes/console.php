86b04b3dc0aa5eeeda370085bd4da6227781c73b
faldy03 <m.fauzan.faldy17@gmail.com>
2026-07-31 22:30:51 +0700

    chore: schedule photos:clean-expired to run every five minutes instead of hourly

--- routes/console.php
+++ routes/console.php
@@ -7,5 +7,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Jadwalkan pembersihan foto kedaluwarsa secara berkala setiap jam
-Schedule::command('photos:clean-expired')->hourly();
+// Jadwalkan pembersihan foto kedaluwarsa secara berkala setiap 5 menit
+Schedule::command('photos:clean-expired')->everyFiveMinutes();
